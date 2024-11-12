b849ecccc0b89101a6ff2ef6503d2819cbd45281
Hakan ÖZER <hakanozer02@gmail.com>
2024-11-12 13:24:38 +0300

    Product Client

--- Product/src/main/java/com/works/ProductApplication.java
+++ Product/src/main/java/com/works/ProductApplication.java
@@ -2,12 +2,14 @@ package com.works;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 //@ComponentScan(basePackages = {""})
 @EnableJpaAuditing
+@EnableDiscoveryClient
 public class ProductApplication {
 
     public static void main(String[] args) {
